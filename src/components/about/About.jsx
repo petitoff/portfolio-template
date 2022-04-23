ca327169501f29104139b03ec37d765703cb5916
Błażej Domagała <petitoff@outlook.com>
2022-04-23 15:49:55 +0200

    Adding components and connecting them to App.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const about = () => {
-  return <div>about</div>;
+const About = () => {
+  return <div>About</div>;
 };
 
-export default about;
+export default About;
