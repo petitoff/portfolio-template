5b545674ff0a8ee16a39d572fff7e5125759d03a
Błażej Domagała <petitoff@outlook.com>
2022-04-25 18:50:25 +0000

    Adding socials
    Adding icons and links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href='htt[s://linkedin.com' target='_blank'></a>
-      <a href='htt[s://github.com' target='_blank'></a>
+      <a href='https://linkedin.com' target='_blank'>
+        <BsLinkedin />
+      </a>
+      <a href='https://github.com' target='_blank'>
+        <BsGithub />
+      </a>
     </div>
   );
 };
