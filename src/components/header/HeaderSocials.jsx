f41b3faf528593008d91ba2a22e3f66b8d689581
Błażej Domagała <petitoff@outlook.com>
2022-04-25 18:18:26 +0000

    Create HeaderSocials.jsx

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href='htt[s://linkedin.com' target='_blank'></a>
+      <a href='htt[s://github.com' target='_blank'></a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
