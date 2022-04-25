028904b4bf6f67418b36aee74f01b07575f1e57a
Błażej Domagała <petitoff@outlook.com>
2022-04-25 18:18:39 +0000

    Add header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import './header.css';
+import CTA from './CTA';
+import mePhoto from '../../assets/me.png';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Błażej Domagała</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        <div className='me'>
+          <img src={mePhoto} alt='' />
+        </div>
+
+        <a href='#contact' className='scroll__down'>
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
