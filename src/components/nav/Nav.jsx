3786bf0bba9a8962e8b3752becc5607a26c48aa5
petitoff <petitoff@outlook.com>
2022-04-30 19:04:55 +0200

    Add icon and click to section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import './nav.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-icons/ai';
+import { BsFillJournalBookmarkFill } from 'react-icons/bs';
+import { RiServiceLine } from 'react-icons/ri';
+import { BiMessageSquareDetail } from 'react-icons/bi';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <a href='#'>
+        <AiOutlineHome />
+      </a>
+      <a href='#about'>
+        <AiOutlineUser />
+      </a>
+      <a href='#experience'>
+        <BsFillJournalBookmarkFill />
+      </a>
+      <a href='#services'>
+        <RiServiceLine />
+      </a>
+      <a href='#contact'>
+        <BiMessageSquareDetail />
+      </a>
+    </div>
+  );
 };
 
 export default Nav;
