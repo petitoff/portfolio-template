c0a319f423733e73afc05b5d049acd117872ba34
petitoff <petitoff@outlook.com>
2022-05-01 12:57:08 +0200

    Add section text and icon

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import './about.css';
+import ME from '../../assets/me-about.jpg';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 const About = () => {
-  return <section id='about'>About</section>;
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='About Image' />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Work</small>
+            </article>
+          </div>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+          </div>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum qui
+            quaerat laboriosam similique dicta natus explicabo libero molestiae
+            fugit dolor suscipit, repellat consequuntur dignissimos animi
+            inventore eos, ipsum soluta rem!
+          </p>
+          <a href='#contact' className='btn btn-primary'>
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
