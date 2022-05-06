a5b510590302b66ff3f72a5dd1e4f58cca4190fe
petitoff <petitoff@outlook.com>
2022-05-06 22:02:00 +0200

    Service section component.

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import './services.css';
+import Card from './Card';
 
 const Services = () => {
-  return <section id='services'>Services</section>;
+  return (
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+        <Card
+          title={'UI/UX'}
+          texts={[
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          ]}
+        />
+
+        <Card
+          title={'Web Development'}
+          texts={[
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          ]}
+        />
+
+        <Card
+          title={'Content Creation'}
+          texts={[
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          ]}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Services;
