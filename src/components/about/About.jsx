52d5ffb37a1f6a19a674fd0734b5842300ca0222
Błażej Domagała <petitoff@outlook.com>
2022-05-07 21:12:26 +0200

    Supplementing the section about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,6 +4,8 @@ import ME from '../../assets/me-about.jpg';
 import { FaAward } from 'react-icons/fa';
 import { FiUsers } from 'react-icons/fi';
 import { VscFolderLibrary } from 'react-icons/vsc';
+import { FaUniversity } from 'react-icons/fa';
+import { GiMaterialsScience } from 'react-icons/gi';
 
 import AboutCard from './AboutCard';
 
@@ -25,27 +27,38 @@ const About = () => {
             <AboutCard
               icon={FaAward}
               h5Text={'Experience'}
-              smallText={'3+ Years Work'}
+              smallText={'1+ Year Work'}
             ></AboutCard>
 
             <AboutCard
               icon={FiUsers}
-              h5Text={'Clients'}
-              smallText={'200+ Worldwide'}
+              h5Text={'Courses'}
+              smallText={'10+ Completed'}
             ></AboutCard>
 
             <AboutCard
               icon={VscFolderLibrary}
               h5Text={'Projects'}
-              smallText={'80+ Completed'}
+              smallText={'3+ Completed'}
+            ></AboutCard>
+
+            <AboutCard
+              icon={FaUniversity}
+              h5Text={'Student'}
+              smallText={'Engineering studies'}
+            ></AboutCard>
+
+            <AboutCard
+              icon={GiMaterialsScience}
+              h5Text={'Algorithm'}
+              smallText={'+30 resolved problems'}
             ></AboutCard>
           </div>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum qui
-            quaerat laboriosam similique dicta natus explicabo libero molestiae
-            fugit dolor suscipit, repellat consequuntur dignissimos animi
-            inventore eos, ipsum soluta rem!
+            I am a student of the University of Opole in the IT institute of
+            engineering studies. I specialize in creating responsive and
+            scalable websites and programs for the windows platform.
           </p>
           <a href='#contact' className='btn btn-primary'>
             Let's Talk
