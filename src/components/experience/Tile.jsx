a77067b3bbaee403df0fc929d1a16ff9b4a7f116
Błażej Domagała <petitoff@outlook.com>
2022-05-07 21:45:27 +0200

    Create Tile.jsx

---
+++ src/components/experience/Tile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsPatchCheckFill } from 'react-icons/bs';
+import ExperienceDetail from './ExperienceDetail';
+
+function Tile(props) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <div className='experience__content'>
+        {props.nameTool.map((n, i) => (
+          <ExperienceDetail key={i} title={n} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Tile;
