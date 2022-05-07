6c6a13a9cc7313b84270656472ceda63f21039c7
petitoff <petitoff@outlook.com>
2022-05-07 12:01:51 +0200

    Create a portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import './Portfolio.css';
+import Items from './Items';
+import Img1 from '../../assets/portfolio1.jpg';
 
 const Portfolio = () => {
-  return <section>Portfolio</section>;
+  return (
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <Items
+          title='This is a portfolio item title'
+          img={Img1}
+          firstLinkTitle='GitHub'
+          secondLinkTitle='Live Demo'
+        />
+
+        <Items
+          title='This is a portfolio item title'
+          img={Img1}
+          firstLinkTitle='GitHub'
+          secondLinkTitle='Live Demo'
+        />
+
+        <Items
+          title='This is a portfolio item title'
+          img={Img1}
+          firstLinkTitle='GitHub'
+          secondLinkTitle='Live Demo'
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
