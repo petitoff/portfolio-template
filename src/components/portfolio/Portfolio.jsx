9bd5efe986b9a0d14f308642790d02da0c38b4d4
petitoff <petitoff@outlook.com>
2022-05-07 13:19:07 +0200

    Add data list of object with data about projects to be display. Add map method to auto clone component.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,33 +3,40 @@ import './Portfolio.css';
 import Items from './Items';
 import Img1 from '../../assets/portfolio1.jpg';
 
+const data = [
+  {
+    image: Img1,
+    title: 'This is a title',
+    github: 'https://github.com/petitoff',
+    demo: 'https://google.com',
+  },
+  {
+    image: Img1,
+    title: 'This is a title',
+    github: 'https://github.com/petitoff',
+    demo: 'https://google.com',
+  },
+];
+
 const Portfolio = () => {
+  let id = -1;
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-        <Items
-          title='This is a portfolio item title'
-          img={Img1}
-          firstLinkTitle='GitHub'
-          secondLinkTitle='Live Demo'
-        />
-
-        <Items
-          title='This is a portfolio item title'
-          img={Img1}
-          firstLinkTitle='GitHub'
-          secondLinkTitle='Live Demo'
-        />
-
-        <Items
-          title='This is a portfolio item title'
-          img={Img1}
-          firstLinkTitle='GitHub'
-          secondLinkTitle='Live Demo'
-        />
+        {data.map(({ image, title, github, demo }) => (
+          <Items
+            key={id++}
+            title={title}
+            img={image}
+            firstLinkTitle='GitHub'
+            firstLink={github}
+            secondLinkTitle='Live Demo'
+            secondLink={demo}
+          />
+        ))}
       </div>
     </section>
   );
