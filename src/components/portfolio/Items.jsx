e008d4abc473ddd51d1740333a9ba5bfee8c088e
petitoff <petitoff@outlook.com>
2022-05-07 13:17:51 +0200

    Add noreferrer and p tag for description projects

--- src/components/portfolio/Items.jsx
+++ src/components/portfolio/Items.jsx
@@ -7,13 +7,29 @@ const Items = (props) => {
         <img src={props.img} alt='' />
       </div>
       <h3>{props.title}</h3>
+      {/* <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+        ratione voluptas aspernatur a vitae at, exercitationem ullam aliquid
+        veritatis sequi, explicabo <br />
+        autem beatae vel soluta eos delectus amet eveniet. Eligendi.
+      </p> */}
 
       <div className='portfolio__item-cta'>
-        <a href={props.link} className='btn' target='_blank'>
+        <a
+          href={props.firstLink}
+          rel='noreferrer'
+          className='btn'
+          target='_blank'
+        >
           {props.firstLinkTitle}
         </a>
 
-        <a href={props.link} className='btn btn-primary' target='_blank'>
+        <a
+          href={props.secondLink}
+          rel='noreferrer'
+          className='btn btn-primary'
+          target='_blank'
+        >
           {props.secondLinkTitle}
         </a>
       </div>
