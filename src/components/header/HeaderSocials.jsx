665ba0224fb15e47f0b23a6d1e3106c76a93e967
petitoff <petitoff@outlook.com>
2022-05-07 13:17:04 +0200

    Add link to profile and add noreferrer

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,10 +5,14 @@ import { BsGithub } from 'react-icons/bs';
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href='https://linkedin.com/in/błażej-domagała' target='_blank'>
+      <a
+        href='https://linkedin.com/in/błażej-domagała'
+        rel='noreferrer'
+        target='_blank'
+      >
         <BsLinkedin />
       </a>
-      <a href='https://github.com/petitoff' target='_blank'>
+      <a href='https://github.com/petitoff' rel='noreferrer' target='_blank'>
         <BsGithub />
       </a>
     </div>
