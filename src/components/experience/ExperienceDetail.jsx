afa27ede8ef01209a8cea74857f88c2131edea6b
Błażej Domagała <petitoff@outlook.com>
2022-05-07 21:13:51 +0200

    Removal of knowledge of tools

--- src/components/experience/ExperienceDetail.jsx
+++ src/components/experience/ExperienceDetail.jsx
@@ -8,7 +8,7 @@ const ExperienceDetail = (props) => {
         <BsPatchCheckFill className='experience__details-icon' />
         <div>
           <h4>{props.title}</h4>
-          <small className='text-light'>Experienced</small>
+          {/* <small className='text-light'>Click!</small> */}
         </div>
       </article>
     </div>
