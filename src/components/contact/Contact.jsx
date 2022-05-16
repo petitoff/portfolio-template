a8c43409b1e4a87eb2d402f0e0c95cc88e586c03
Błażej Domagała <petitoff@outlook.com>
2022-05-16 21:40:30 +0200

    Import template tile for contact form
    Import template for mail, fb and other

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Contact.css';
 import { MdOutlineMail } from 'react-icons/md';
+import Tile from './Tile';
 
 const Contact = () => {
   return <section id='contact'>Contact</section>;
