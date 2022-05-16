e74dd2713472a30bb9a4aac9d9d56f3e6c302804
Błażej Domagała <petitoff@outlook.com>
2022-05-16 21:41:06 +0200

    Add data for tile for using map

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,6 +3,15 @@ import './Contact.css';
 import { MdOutlineMail } from 'react-icons/md';
 import Tile from './Tile';
 
+const data = [
+  {
+    title: 'Email',
+    name: 'petitoff@outlook.com',
+    link: 'mailto:petitoff@outlook.com',
+    linkText: 'Send a mail',
+  },
+];
+
 const Contact = () => {
   return <section id='contact'>Contact</section>;
 };
