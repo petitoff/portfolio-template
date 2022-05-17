a3601a87e67a524b5c339850958cab09a8f19e3d
Błażej Domagała <petitoff@outlook.com>
2022-05-17 17:31:40 +0200

    create footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,50 @@ import { AiFillGithub } from 'react-icons/ai';
 import { FaFacebookF } from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href='#' className='footer__logo'>
+        PETITOFF
+      </a>
+
+      <ul className='permalinks'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#experience'>Experience</a>
+        </li>
+        <li>
+          <a href='#services'>Services</a>
+        </li>
+        <li>
+          <a href='#portfolio'>Portfolio</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://github.com/petitoff'>
+          <AiFillGithub />
+        </a>
+        <a href='https://github.com/petitoff'>
+          <AiFillGithub />
+        </a>
+        <a href='https://github.com/petitoff'>
+          <FaFacebookF />
+        </a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; petitoff. Create with {'❤'}</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
